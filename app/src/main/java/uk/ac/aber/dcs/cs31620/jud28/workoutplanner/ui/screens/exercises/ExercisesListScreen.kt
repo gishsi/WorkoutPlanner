551f51c69b5ff29de67add2ab43fa6a5e593d8eb
gishsi <spokosalo@gmail.com>
2023-12-29 11:31:58 +0100

    Squashed commit of the following:
    commit b4ad021fc2c4a6c1976b16a778bdc1ddbf1d960b
    Author: gishsi <spokosalo@gmail.com>
    Date:   Fri Dec 29 11:28:14 2023 +0100
    
        exersices list
    
    commit 900f20465fa7712a4869939e6d0e69af2622da8b
    Author: gishsi <spokosalo@gmail.com>
    Date:   Fri Dec 29 11:23:57 2023 +0100
    
        no workouts variant

--- app/src/main/java/uk/ac/aber/dcs/cs31620/jud28/workoutplanner/ui/screens/exercises/ExercisesListScreen.kt
+++ app/src/main/java/uk/ac/aber/dcs/cs31620/jud28/workoutplanner/ui/screens/exercises/ExercisesListScreen.kt
@@ -1,6 +1,9 @@
 package uk.ac.aber.dcs.cs31620.jud28.workoutplanner.ui.screens.exercises
 
 import android.util.Log
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -9,6 +12,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
@@ -19,12 +23,16 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
@@ -98,64 +106,116 @@ fun ExercisesListContent(
             .padding(32.dp)
             .fillMaxHeight()
     ) {
-        LazyColumn(
-            modifier = Modifier.padding(bottom = 32.dp)
-        ) {
-            mapOfSorted.forEach { (key, exercises) ->
-                item {
+        if(exercisesList.isEmpty()) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier.fillMaxSize()
+            ) {
+                Text(
+                    text = "No exercises yet",
+                    fontWeight = FontWeight.Bold,
+                    style = MaterialTheme.typography.titleLarge,
+                )
+                Text(
+                    text = "Click below to add a new exercise",
+                    color = MaterialTheme.colorScheme.onBackground
+                )
+
+                Spacer(modifier = Modifier.height(32.dp))
+
+                Image(
+                    painter = painterResource(R.drawable.undraw_fitness_tracker_3033_1),
+                    contentDescription = stringResource(id = R.string.vec_graphics_no_exercises),
+                    modifier = Modifier.padding(horizontal = 64.dp, vertical = 0.dp)
+                )
+
+                Spacer(modifier = Modifier.height(64.dp))
+
+                Button(
+                    onClick = {
+                        navController.navigate(Screen.ExerciseAdd.route) {
+                            popUpTo(navController.graph.findStartDestination().id) {
+                                saveState = true
+                            }
+                            launchSingleTop = true
+                            restoreState = true
+                        }
+                    },
+                    modifier = Modifier
+                        .clip(RoundedCornerShape(8.dp))
+                        .background(MaterialTheme.colorScheme.primary)
+                        .padding(horizontal = 32.dp, vertical = 16.dp),
+                ) {
                     Text(
-                        text = key.toString(),
-                        style = MaterialTheme.typography.titleLarge,
+                        text = "Add an exercise",
+                        color = MaterialTheme.colorScheme.onPrimary,
                         fontWeight = FontWeight.Bold,
-                        fontStyle = FontStyle.Italic,
-                        modifier = Modifier.padding(4.dp)
+                        fontSize = 24.sp,
                     )
-                    for (exercise in exercises) {
-                        ExerciseCard(
-                            exercise = exercise,
-                            editAction = {
-                                Log.d("EXE_LIST", "Editing an exercise [${it.id}]")
-
-                                val serializedExercise = Gson().toJson(it)
-
-                                navController.navigate(
-                                    Screen.ExerciseEdit.route.replace(
-                                        "{exercise}",
-                                        serializedExercise
-                                    )
-                                )
-                            },
-                            deleteAction = {
-                                Log.d("EXE_LIST", "Deleting an exercise [${it.name}]")
-                                onDelete(exercise)
-                            },
-                            showAction = true,
-                        )
-                    }
                 }
+            }
+        } else {
+            LazyColumn(
+                modifier = Modifier.padding(bottom = 32.dp)
+            ) {
+                mapOfSorted.forEach { (key, exercises) ->
+                    item {
+                        Text(
+                            text = key.toString(),
+                            style = MaterialTheme.typography.titleLarge,
+                            fontWeight = FontWeight.Bold,
+                            fontStyle = FontStyle.Italic,
+                            modifier = Modifier.padding(4.dp)
+                        )
+                        for (exercise in exercises) {
+                            ExerciseCard(
+                                exercise = exercise,
+                                editAction = {
+                                    Log.d("EXE_LIST", "Editing an exercise [${it.id}]")
 
+                                    val serializedExercise = Gson().toJson(it)
+
+                                    navController.navigate(
+                                        Screen.ExerciseEdit.route.replace(
+                                            "{exercise}",
+                                            serializedExercise
+                                        )
+                                    )
+                                },
+                                deleteAction = {
+                                    Log.d("EXE_LIST", "Deleting an exercise [${it.name}]")
+                                    onDelete(exercise)
+                                },
+                                showAction = true,
+                            )
+                        }
+                    }
+
+                }
+            }
+
+            Spacer(
+                modifier = Modifier
+                    .height(8.dp)
+                    .weight(2F)
+            )
+
+            Button(
+                modifier = Modifier.fillMaxWidth(), onClick = {
+                    navController.navigate(Screen.ExerciseAdd.route) {
+                        popUpTo(navController.graph.findStartDestination().id) {
+                            saveState = true
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                }) {
+                Text(text = "Add an exercise")
+                Icon(imageVector = Icons.Filled.Add, contentDescription = stringResource(R.string.add))
             }
         }
 
-        Spacer(
-            modifier = Modifier
-                .height(8.dp)
-                .weight(2F)
-        )
-
-        Button(
-            modifier = Modifier.fillMaxWidth(), onClick = {
-                navController.navigate(Screen.ExerciseAdd.route) {
-                    popUpTo(navController.graph.findStartDestination().id) {
-                        saveState = true
-                    }
-                    launchSingleTop = true
-                    restoreState = true
-                }
-            }) {
-            Text(text = "Add an exercise")
-            Icon(imageVector = Icons.Filled.Add, contentDescription = stringResource(R.string.add))
-        }
     }
 
 }
@@ -166,11 +226,29 @@ fun ExercisesListContent(
 fun ExerciseListScreenPreview() {
     val navHostController = rememberNavController()
     WorkoutPlannerTheme(dynamicColor = false) {
-        ExercisesListContent(
-            navHostController, listOf(
-                Exercise(0, "Dips", 3, 10, 10F, R.drawable.dips.toString()),
+        Surface {
+
+
+            ExercisesListContent(
+                navHostController, listOf(
+                    Exercise(0, "Dips", 3, 10, 10F, R.drawable.dips.toString()),
+                )
             )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun NoExerciseListScreenPreview() {
+    val navHostController = rememberNavController()
+    WorkoutPlannerTheme(dynamicColor = false) {
+        Surface {
+
+        ExercisesListContent(
+            navHostController, listOf()
         )
+        }
     }
 }
 
