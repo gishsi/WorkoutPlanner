11d832f9e0081038a28ac7cf9f5941ce1baf18d9
gishsi <spokosalo@gmail.com>
2023-12-30 18:07:06 +0100

    Update DeleteWorkoutConfirmationDialog.kt

--- app/src/main/java/uk/ac/aber/dcs/cs31620/jud28/workoutplanner/ui/screens/workouts/components/DeleteWorkoutConfirmationDialog.kt
+++ app/src/main/java/uk/ac/aber/dcs/cs31620/jud28/workoutplanner/ui/screens/workouts/components/DeleteWorkoutConfirmationDialog.kt
@@ -7,6 +7,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
@@ -55,7 +56,9 @@ fun DeleteWorkoutConfirmationDialog(
             TextButton(onClick = onDeleteConfirm) {
                 Text(text = stringResource(R.string.remove))
             }
-        })
+        },
+        containerColor = Color.White
+    )
 }
 
 @Preview
